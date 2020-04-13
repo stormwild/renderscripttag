6665300a1c15600606dad8998ce1595784070f8a
Alexander R. Torrijos <artorrijos@gmail.com>
2020-04-13 12:48:48 +0800

    Render script tag

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const danger = '<script>alert("Hi")</script>'
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div dangerouslySetInnerHTML={{ __html: danger}}></div>
     </div>
   );
 }
